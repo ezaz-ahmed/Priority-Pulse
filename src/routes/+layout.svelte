93e6055f987b5cb9879427139092ae4d959f8238
Ezaz Ahmed <ezaz.dev@gmail.com>
2023-01-09 13:10:01 +0600

    Tailwind and DaisyUi add

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost normal-case text-xl">daisyUI</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/test">Item 1</a></li>
+			<li><a href="/fest">Item 2</a></li>
+			<li><a href="/den">Item 3</a></li>
+		</ul>
+	</div>
+</div>
+<slot />
